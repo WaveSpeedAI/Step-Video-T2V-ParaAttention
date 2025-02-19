966d4e57979079f97887801f840788f813aa9e65
chengzeyi <ichengzeyi@gmail.com>
2025-02-19 07:22:49 +0000

    support 6 gpus

--- stepvideo/para_attn/context_parallel.py
+++ stepvideo/para_attn/context_parallel.py
@@ -55,13 +55,13 @@ def parallelize_transformer(transformer: StepVideoModel, *, mesh=None):
             attn_mask = DP.get_assigned_chunk(attn_mask, dim=0, group=batch_mesh)
 
         encoder_hidden_states_len = encoder_hidden_states.size(-2)
-        if encoder_hidden_states_len % 8 != 0:
+        if encoder_hidden_states_len % 24 != 0:
             encoder_hidden_states = torch.nn.functional.pad(
-                encoder_hidden_states, (0, 0, 0, 8 - encoder_hidden_states_len % 8)
+                encoder_hidden_states, (0, 0, 0, 24 - encoder_hidden_states_len % 24)
             )
             if attn_mask is not None:
                 attn_mask = torch.nn.functional.pad(
-                    attn_mask, (0, 8 - encoder_hidden_states_len % 8)
+                    attn_mask, (0, 24 - encoder_hidden_states_len % 24)
                 )
 
         encoder_hidden_states = DP.get_assigned_chunk(
